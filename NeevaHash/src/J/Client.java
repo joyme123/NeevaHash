e687c8756f89678047e0f5fd0fa8cb22c92bc3fb
joyme123 <786185931@qq.com>
2016-09-12 23:12:34 +0800

    在大神的提示下，和亲爱的一起写好了核心的算法，还缺少对输入的封装

--- NeevaHash/src/J/Client.java
+++ NeevaHash/src/J/Client.java
@@ -2,7 +2,7 @@ package J;
 
 public class Client {
 	public static void main(String[] args){
-		byte[] a = {(byte)0xab,(byte)0x80,(byte)0x00,(byte)0x01};
+		byte[] a = {(byte)0xab,(byte)0x80,(byte)0x00,(byte)0x01};	//这里输入的是ab,还缺少对输入的封装
 		Neeva neeva = new Neeva();
 		neeva.init(a);
 		neeva.fFunction();
